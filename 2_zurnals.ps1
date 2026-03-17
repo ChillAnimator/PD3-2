044f12af35c140256feb6ed4a98e65a71d6c338e
ChillAnimator <arturr2016@gmail.com>
2026-03-17 14:30:13 +0200

    Update 2_zurnals.ps1

--- 2_zurnals.ps1
+++ 2_zurnals.ps1
@@ -1,14 +1,14 @@
-﻿$days = 3
+$days = 3
 $logName = "Application"
 $docPath = [Environment]::GetFolderPath("MyDocuments")
 $filePath = Join-Path -Path $docPath -ChildPath "Warnings.txt"
 $startTime = (Get-Date).AddDays(-$days)
-Get-WinEvent -LogName $logName -StartTime $startTime |
-	Where-Object { $_.LevelDisplayName -eq "Warning" } |
+Get-EventLog -LogName $logName -After $startTime |
+	Where-Object { $_.EntryType -eq "Warning" } |
 	Out-File -FilePath $filePath -Force
 "`n`n--- Top 3 Warning Sources (Count) ---`n" | Out-File -FilePath $filePath -Append
-Get-WinEvent -LogName $logName -startTime $startTime |
-	Where-Object { $_.LevelDisplayName -eq "Warning" } |
+Get-EventLog -LogName $logName -After $startTime |
+	Where-Object { $_.EntryType -eq "Warning" } |
 	Group-Object -Property Source |
 	Sort-Object -Property Count -Descending |
 	Select-Object -First 3 |
