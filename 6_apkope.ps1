d2cde79a6c8cc3afa00e9232bdd5cb7b29279b45
ChillAnimator <arturr2016@gmail.com>
2026-03-19 10:18:33 +0200

    Update 6_apkope.ps1

--- 6_apkope.ps1
+++ 6_apkope.ps1
@@ -1,1 +1,8 @@
-﻿
+$name = @("Spooler"﻿, "wuauserv")
+$logfile = "$env:USERPROFILE\Documents\Servisi.log"
+$time = get-date -format "yyyy-MM-dd HH:mm:ss"
+foreach ($svc in $name) {
+  $status = (get-service -name $svc -erroraction SilentlyContinue).Status
+  if (-not $status) { $status = "Nav atrasts" }
+  "$time Serviss $svc ir $status." | out-file -filepath $logfile -append
+}
