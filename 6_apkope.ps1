1c5a9f80aa458d73cc47bd23adbb98e4fda347f7
ChillAnimator <arturr2016@gmail.com>
2026-03-19 10:41:19 +0200

    Add files via upload

---
+++ 6_apkope.ps1
@@ -0,0 +1,13 @@
+﻿$name = @("Spooler", "wuauserv")
+$log = "Services.log"
+foreach ($Sname in $name) {
+	$name = get-service -name $Sname -erroraction SilentlyContinue
+	if ($name) {
+		$time = get-date -format "yyyy-MM-dd HH:mm:ss"
+		$entry = "[$time] Serviss [$($name.Name)] ir [$($name.Status)]."
+		add-content -path $log -value $entry
+	}
+	else {
+		write-warning "Serviss $Sname not found šaja sistema!"
+	}
+}
