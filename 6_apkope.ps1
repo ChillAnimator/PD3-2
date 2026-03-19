95dc478628f03404beb53dc1aa3e2c8d448aded3
ChillAnimator <arturr2016@gmail.com>
2026-03-19 10:21:24 +0200

    Delete 6_apkope.ps1

--- 6_apkope.ps1
+++
@@ -1,8 +0,0 @@
-$name = @("Spooler"﻿, "wuauserv")
-$logfile = "$env:USERPROFILE\Documents\Servisi.log"
-$time = get-date -format "yyyy-MM-dd HH:mm:ss"
-foreach ($svc in $name) {
-  $status = (get-service -name $svc -erroraction SilentlyContinue).Status
-  if (-not $status) { $status = "Nav atrasts" }
-  "$time Serviss $svc ir $status." | out-file -filepath $logfile -append
-}
